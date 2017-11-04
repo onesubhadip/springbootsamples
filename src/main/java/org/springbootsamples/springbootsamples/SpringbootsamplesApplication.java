38ee12b40d2da9e43d9303f87c2852ad941886f6
Subhadip Dey <onesubhadip@gmail.com>
2017-11-04 20:10:18 +0530

    Initial Commit

---
+++ src/main/java/org/springbootsamples/springbootsamples/SpringbootsamplesApplication.java
@@ -0,0 +1,12 @@
+package org.springbootsamples.springbootsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsamplesApplication.class, args);
+	}
+}
---
+++ src/test/java/org/springbootsamples/springbootsamples/SpringbootsamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.springbootsamples.springbootsamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootsamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
